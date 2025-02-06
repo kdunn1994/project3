6051fecb7c4a202dfc650a6db692989c4d97c36c
Kevin Dunn <kdunn1@stu.southtexascollege.edu>
2025-02-05 18:51:53 -0600

    first commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="content">
+    <div>
+      <h1 class="main-title"> A Simple Count Down Timer</h1>
+      <h2 class="center-text">by Kevin Dunn</h2>
+  </div>
+  <div class="formItems">
+    <input v-model="timeInput" type="number" placeholder="Enter Seconds" />
+    <button @click="startTimer" type="button">Start Timer</button>
+  </div>
+  <div id="timerDisplay" class="timerDisplay">{{ timeDisplay }}</div>
+</div>
+
+</template>
+
